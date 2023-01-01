f1294962ce1a0060de40290b9bd91b5acfd467fa
Dan Klishch <danilklishch@gmail.com>
2023-01-01 16:48:05 +0500

    API: Refactor routes/kims to follow the newest "guidelines"

--- api/routes/kims.cc
+++ api/routes/kims.cc
@@ -61,32 +61,6 @@ coro<void> handle_kim_get_editable(fcgx::request_t *r) {
 	utils::ok(r, msg);
 }
 
-// clang-format off
-const char KIM_UPDATE_SQL[] =
-	"INSERT INTO kims ("
-	  "id, name, start_time, end_time, "
-	  "duration, virtual, exam"
-	") "
-	"VALUES "
-	  "("
-	    "$1, "
-	    "(CASE WHEN $3 THEN $2::text ELSE NULL END), "
-	    "(CASE WHEN $5 THEN $4::timestamp ELSE NULL END), "
-	    "(CASE WHEN $7 THEN $6::timestamp ELSE NULL END), "
-	    "(CASE WHEN $9 THEN $8::bigint ELSE NULL END), "
-	    "(CASE WHEN $11 THEN $10::bool ELSE NULL END), "
-	    "(CASE WHEN $13 THEN $12::bool ELSE NULL END)"
-	  ")"
-	"ON CONFLICT (id) DO UPDATE SET "
-	  "name = (CASE WHEN $3 THEN excluded ELSE kims END).name, "
-	  "start_time = (CASE WHEN $5 THEN excluded ELSE kims END).start_time, "
-	  "end_time = (CASE WHEN $7 THEN excluded ELSE kims END).end_time, "
-	  "duration = (CASE WHEN $9 THEN excluded ELSE kims END).duration, "
-	  "virtual = (CASE WHEN $11 THEN excluded ELSE kims END).virtual, "
-	  "exam = (CASE WHEN $13 THEN excluded ELSE kims END).exam "
-	"RETURNING (xmax = 0)";
-// clang-format on
-
 coro<void> handle_kim_update(fcgx::request_t *r) {
 	auto db = co_await async::pq::connection_pool::local->get_connection();
 	auto session = co_await routes::require_auth(db, r, routes::PERM_NOT_STUDENT);
@@ -96,31 +70,8 @@ coro<void> handle_kim_update(fcgx::request_t *r) {
 		utils::err(r, api::INVALID_QUERY);
 	}
 
-	// Protect from overflows to be on the save side.
-	auto check_interval = [r](int64_t what, int64_t lo, int64_t hi) {
-		if (what < lo || what > hi) {
-			utils::err(r, api::INVALID_QUERY);
-		}
-	};
-	// 5e12ms as ns can fit into int64_t.
-	// check_interval(kim.start_time(), -5e12, 5e12);
-	// check_interval(kim.end_time(), -5e12, 5e12);
-	// check_interval(kim.duration(), -5e12, 5e12);
-
 	co_await db.transaction();
 
-	// auto start_time = async::pq::timestamp(std::chrono::milliseconds(kim.start_time())),
-	// 	 end_time = async::pq::timestamp(std::chrono::milliseconds(kim.end_time()));
-	// auto [is_kim_inserted] =
-	// 	(co_await db.exec(KIM_UPDATE_SQL, kim.id(), kim.name(), kim.has_name(), start_time,
-	// 					  kim.has_start_time(), end_time, kim.has_end_time(), kim.duration(),
-	// 					  kim.has_duration(), kim.is_virtual(), kim.has_is_virtual(), kim.is_exam(),
-	// 					  kim.has_is_exam()))
-	// 		.expect1<bool>();
-
-	// if (is_kim_inserted && !is_id_owned_by(session, kim.id())) {
-	// 	utils::err(r, api::EXTREMELY_SORRY);
-	// }
 	bool is_kim_inserted = false;
 
 	// Process task swaps
@@ -132,12 +83,8 @@ coro<void> handle_kim_update(fcgx::request_t *r) {
 		}
 	}
 
-	auto q =
-		(co_await db.exec("SELECT task_id, pos FROM unnest($1::bigint[]) AS ids JOIN kims_tasks ON "
-						  "task_id = ids WHERE kim_id = $2 ORDER BY (task_id, pos) FOR UPDATE",
-						  task_ids, kim.id()));
 	std::map<int64_t, int> db_positions;
-	for (auto [key, value] : q.iter<int64_t, int>()) {
+	for (auto [key, value] : co_await db.exec(LOCK_MENTIONED_TASKS_REQUEST)) {
 		db_positions[key] = value;
 	}
 
@@ -163,14 +110,10 @@ coro<void> handle_kim_update(fcgx::request_t *r) {
 
 	if (is_kim_inserted) {
 		new_start_pos = 0, new_end_pos = introduced_cnt;
-		co_await db.exec("INSERT INTO kims_tasks VALUES ($1, NULL, $2)", kim.id(), new_end_pos);
+		co_await db.exec(INSERT_TASK_COUNT_RECORD_REQUEST);
 	}
 	if (introduced_cnt && !is_kim_inserted) {
-		std::tie(new_end_pos) =
-			(co_await db.exec("UPDATE kims_tasks SET pos = pos + $1 WHERE kim_id = "
-							  "$2 AND task_id IS NULL RETURNING pos",
-							  introduced_cnt, kim.id()))
-				.expect1<int>();
+		std::tie(new_end_pos) = (co_await db.exec(UPDATE_TASK_COUNT_RECORD_REQUEST)).expect1();
 		new_start_pos = new_end_pos - introduced_cnt;
 	}
 	for (int pos : introduced_pos) {
@@ -193,19 +136,12 @@ coro<void> handle_kim_update(fcgx::request_t *r) {
 		}
 	}
 	if (delete_ids.size()) {
-		co_await db.exec(
-			"DELETE FROM kims_tasks USING unnest($1::bigint[]) AS ids WHERE kim_id = $2 AND "
-			"task_id = ids",
-			delete_ids, kim.id());
+		co_await db.exec(DELETE_TASKS_REQUEST);
 	}
 
 	// 5) Finally swap
 	if (swap_ids.size()) {
-		co_await db.exec(
-			"INSERT INTO kims_tasks VALUES ($1, unnest($2::bigint[]), unnest($3::int[])) ON "
-			"CONFLICT"
-			" (kim_id, task_id) DO UPDATE SET pos = excluded.pos",
-			kim.id(), swap_ids, swap_positions);
+		co_await db.exec(INSERT_AND_SWAP_TASKS_REQUEST);
 	}
 	co_await db.commit();
 
@@ -232,194 +168,3 @@ coro<void> handle_kim_list(fcgx::request_t *r) {
 ROUTE_REGISTER("/kim/$id", handle_kim_get_editable)
 ROUTE_REGISTER("/kim/update", handle_kim_update)
 ROUTE_REGISTER("/kim/list", handle_kim_list)
-
-// static coro<void> handle_kim_list(fcgx::request_t *r) {
-// 	auto db = co_await async::pq::connection_pool::local->get_connection();
-// 	auto session = (co_await routes::require_auth(db, r));
-
-// 	api::UserKimListResponse ans{};
-
-// 	auto q2 = co_await db.exec(
-// 		"SELECT kims.id, kims.name, kims.duration, kims.start_time, kims.end_time, kims.comment,
-// users_kims.state " 		"FROM kims INNER JOIN users_kims ON kims.id IN "
-// 		"(SELECT users_kims.kim_id FROM users_kims WHERE users_kims.user_id = $1) "
-// 		"AND kims.start_time <= CURRENT_TIMESTAMP AND CURRENT_TIMESTAMP <= kims.end_time "
-// 		"AND users_kims.kim_id = kims.id",
-// 		user_id);
-// 	for (auto [id, name, duration, start_time, end_time, comment, state] :
-// 		 q2.iter<int64_t, std::string_view, int, async::pq::timestamp, async::pq::timestamp,
-// std::string_view, int>()) { 		logd("{} {} {} {} {} {} {}", id, name, duration, start_time,
-// end_time, comment, state); 		*ans.add_kim() = {{ 			.id = id, 			.name =
-// std::string(name), .duration = duration, 			.start_time =
-// start_time.time_since_epoch().count(), 			.end_time = end_time.time_since_epoch().count(),
-// .comment = std::string(comment), 			.state = state,
-// 		}};
-// 	}
-
-// 	utils::ok(r, ans);
-// }
-
-// static coro<void> handle_kim_info(fcgx::request_t *r) {
-// 	auto db = co_await async::pq::connection_pool::local->get_connection();
-// 	auto user_id = (co_await routes::require_auth(db, r))->user_id;
-
-// 	auto q2 = co_await db.exec(
-// 		"SELECT kims.name, kims.duration, kims.start_time, "
-// 		"kims.end_time, kims.comment, users_kims.state "
-// 		"FROM kims INNER JOIN users_kims ON "
-// 		"kims.id = $1 AND users_kims.kim_id = $1 AND users_kims.user_id = $2",
-// 		r->params["id"], user_id);
-// 	if (!q2.rows()) {
-// 		utils::ok<api::Kim>(r, {});
-// 		co_return;
-// 	}
-// 	auto [name, duration, start_time, end_time, state] =
-// 		q2.expect1<std::string_view, int64_t, double, double, int>();
-
-// 	api::Kim ans{{
-// 		.id = stoi(r->params["id"]),
-// 		.name = std::string(name),
-// 		.duration = duration,
-// 		.start_time = start_time,
-// 		.end_time = end_time,
-// 		.state = state
-// 	}};
-
-// 	utils::ok(r, ans);
-// }
-
-// static coro<void> handle_kim_tasks(fcgx::request_t *r) {
-// 	auto db = co_await async::pq::connection_pool::local->get_connection();
-// 	co_await routes::require_auth(db, r);
-
-// 	api::KimTaskListResponse ans{};
-
-// 	auto q = co_await db.exec(
-// 		"SELECT kims_tasks.task_id, kims_tasks.pos FROM kims_tasks WHERE "
-// 		"kims_tasks.kim_id = $1",
-// 		r->params["id"]);
-// 	if (!q.rows()) {
-// 		utils::ok<api::KimTaskListResponse>(r, ans);
-// 		co_return;
-// 	}
-// 	for (auto [task_id, pos] : q.iter<int64_t, int>()) {
-// 		api::Task task;
-
-// 		auto q1 = co_await db.exec(
-// 			"SELECT * FROM tasks WHERE id = $1::bigint AND NOT coalesce(deleted, false)",
-// 			r->params["id"]);
-// 		if (q1.rows()) {
-// 			auto [id, task_type, parent, text, tag, answer_rows, answer_cols, answer, deleted] =
-// 				q1.expect1<int64_t, int64_t, int64_t, std::string_view, std::string_view, int, int,
-// 						  std::string_view, bool>();
-
-// 			task = api::Task::initializable_type{
-// 				.id = id,
-// 				.task_type = task_type,
-// 				.parent = parent,
-// 				.text = std::string(text),
-// 				.answer_rows = answer_rows,
-// 				.answer_cols = answer_cols,
-// 				.answer = std::string(answer),
-// 				.tag = std::string(tag),
-// 			};
-
-// 			auto q2 = co_await db.exec(
-// 				"SELECT id, filename, mime_type, hash, shown_to_user FROM task_attachments WHERE
-// task_id
-// =
-// "
-// 				"$1::bigint AND NOT coalesce(deleted, false)",
-// 				r->params["id"]);
-// 			for (auto [attachment_id, filename, mime_type, hash, shown_to_user] :
-// 				 q2.iter<int64_t, std::string_view, std::string_view, std::string_view, bool>()) {
-// 				*task.add_attachments() = {{
-// 					.id = attachment_id,
-// 					.filename = std::string(filename),
-// 					.mime_type = std::string(mime_type),
-// 					.hash = std::string(hash),
-// 					.shown_to_user = shown_to_user,
-// 				}};
-// 			}
-// 		}
-
-// 		*ans.add_tasks() = {{
-// 			.task = task,
-// 			.pos = pos
-// 		}};
-// 	}
-
-// 	utils::ok(r, ans);
-// }
-
-// static coro<void> handle_kim_user_answers(fcgx::request_t *r) {
-// 	auto db = co_await async::pq::connection_pool::local->get_connection();
-// 	co_await routes::require_auth(db, r);
-
-// 	auto cookies_it = r->cookies.find("kege-session");
-// 	const auto &session_id = cookies_it->second;
-
-// 	auto q1 = co_await db.exec(
-// 		"SELECT users.id "
-// 		"FROM (sessions JOIN users ON sessions.user_id = users.id) WHERE sessions.id = $1",
-// 		session_id);
-// 	if (q1.rows() != 1) {
-// 		utils::err(r, api::ACCESS_DENIED);
-// 	}
-// 	auto [user_id] = q1.expect1<int>();
-
-// 	api::KimUserAnswerListResponse ans{};
-
-// 	auto q2 = co_await db.exec(
-// 		"SELECT users_answers.pos, users_answers.answer, users_answers.submit_time "
-// 		"FROM kims_tasks WHERE users_answers.kim_id = $1 AND users_answers.user_id = $2",
-// 		r->params["id"], user_id);
-// 	if (!q2.rows()) {
-// 		utils::ok<api::KimUserAnswerListResponse>(r, ans);
-// 		co_return;
-// 	}
-// 	for (auto [pos, answer, submit_time] :
-// 		 q2.iter<int, std::string_view, int64_t>()) {
-// 		*ans.add_answers() = {{
-// 			.pos = pos,
-// 			.answer = std::string(answer),
-// 			.submit_time = submit_time
-// 		}};
-// 	}
-
-// 	utils::ok(r, ans);
-// }
-
-// static coro<void> handle_update_state(fcgx::request_t *r) {
-// 	auto db = co_await async::pq::connection_pool::local->get_connection();
-// 	co_await routes::require_auth(db, r);
-
-// 	auto cookies_it = r->cookies.find("kege-session");
-// 	const auto &session_id = cookies_it->second;
-
-// 	auto q1 = co_await db.exec(
-// 		"SELECT users.id "
-// 		"FROM (sessions JOIN users ON sessions.user_id = users.id) WHERE sessions.id = $1",
-// 		session_id);
-// 	if (q1.rows() != 1) {
-// 		utils::err(r, api::ACCESS_DENIED);
-// 	}
-// 	auto [user_id] = q1.expect1<int>();
-
-// 	co_await db.transaction();
-
-// 	auto q2 = co_await db.exec(
-// 		"UPDATE users_kims SET state = $2 "
-// 		"WHERE users_kims.kim_id = $1 AND users_kims.user_id = $3",
-// 		r->params["id"], r->params["state"], user_id);
-
-// 	co_await db.commit();
-
-// 	utils::ok(r, utils::empty_payload{});
-// }
-
-// ROUTE_REGISTER("/kim/list", handle_kim_list)
-// ROUTE_REGISTER("/kim/info/$id", handle_kim_info)
-// ROUTE_REGISTER("/kim/tasks/$id", handle_kim_tasks)
-// ROUTE_REGISTER("/kim/user/answers/$id", handle_kim_user_answers)
-// ROUTE_REGISTER("/kim/update/state/$id/$state", handle_update_state)
--- api/routes/kims.sql
+++ api/routes/kims.sql
@@ -34,3 +34,52 @@ FROM
 WHERE
     NOT coalesce(deleted, FALSE);
 
+-- Bump KIM version
+UPDATE
+    kims
+SET
+    token_version = COALESCE(token_version, 0) + 1
+WHERE
+    id = `utils::expect<int64_t>(r, "id")`;
+
+-- Lock mentioned tasks
+SELECT
+    task_id,
+    pos
+FROM
+    unnest(`task_ids`::bigint[]) AS ids
+    JOIN kims_tasks ON task_id = ids
+WHERE
+    kim_id = `kim.id()`
+ORDER BY
+    (task_id,
+        pos)
+FOR UPDATE;
+
+-- Insert task count record
+INSERT INTO kims_tasks
+    VALUES (`kim.id()`, NULL, `new_end_pos`);
+
+-- Update task count record
+UPDATE
+    kims_tasks
+SET
+    pos = pos + `introduced_cnt`
+WHERE
+    kim_id = `kim.id()`
+    AND task_id IS NULL
+RETURNING
+    pos;
+
+-- Delete tasks
+DELETE FROM kims_tasks USING unnest(`delete_ids`::bigint[]) AS ids
+    WHERE kim_id = `kim.id()`
+        AND task_id = ids;
+
+-- Insert and swap tasks
+INSERT INTO kims_tasks
+    VALUES (`kim.id()`, unnest(`swap_ids`::bigint[]), unnest(`swap_positions`::int[]))
+ON CONFLICT (kim_id, task_id)
+    DO UPDATE SET
+        pos = excluded.pos;
+

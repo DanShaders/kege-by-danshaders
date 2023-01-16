9636effcb0c4efefe38a1b508469c477c64026c5
Dan Klishch <danilklishch@gmail.com>
2023-01-16 23:20:09 +0300

    meta: Ensure unique usernames

--- meta/postgres/db.sql
+++ meta/postgres/db.sql
@@ -67,7 +67,9 @@ CREATE TABLE users (
 	display_name text,
 	permissions integer,
 	salt char(64),
-	password char(64) -- = b16encode(sha3_256(password + b16decode(salt)))
+	password char(64), -- = b16encode(sha3_256(password + b16decode(salt)))
+
+	UNIQUE (username)
 );
 
 CREATE TABLE groups (
@@ -146,6 +148,10 @@ INSERT INTO users (username, display_name, permissions, salt, password) VALUES (
 	'e1bdae5584062bf6a4472260180be513e1db4cfe334fec1c67cb5e5eb26452fa'
 );
 
+INSERT INTO groups (display_name) VALUES ('Тестовая группа');
+INSERT INTO users_groups VALUES (1, 3);
+INSERT INTO users_groups VALUES (2, 3);
+
 COPY task_types (id, obsolete, short_name, full_name, grading, scale_factor, deleted) FROM stdin;
 500000	f	1	1. Анализ информационных моделей	1	1	f
 500001	f	2	2. Таблицы истинности логических выражений	1	1	f
@@ -180,31 +186,3 @@ COPY task_types (id, obsolete, short_name, full_name, grading, scale_factor, del
 500030	t	117	117. Обработка целочисленных данных. Проверка делимости	1	1	f
 500031	t	122	122. Анализ программ с циклами и ветвлениями	1	1	f
 \.
-
-INSERT INTO tasks (task_type, task, tag, answer_rows, answer_cols, answer, deleted) VALUES (
-	500026,
-	'Имеется набор данных, состоящий из пар положительных целых чисел. Необходимо выбрать из каждой пары ровно одно число так, чтобы сумма всех выбранных чисел не делилась на 3 и при этом была максимально возможной. Гарантируется, что искомую сумму получить можно. Программа должна напечатать одно число – максимально возможную сумму, соответствующую условиям задачи.<br><br>Входные данные.<br>Даны два входных файла (файл A и файл B), каждый из которых содержит в первой строке количество пар <formula>n</formula> (<formula>1 \le n \le 100\,000</formula>). Каждая из следующих <formula>n</formula> строк содержит два натуральных числа, не превышающих <formula>10\,000</formula>.<br>Пример организации исходных данных во входном файле:<br>	Tab<br><pre>6<br>1 3<br>5 12<br>6 9<br>5 4<br>3 3<br>1 1<br></pre>Для указанных входных данных значением искомой суммы должно быть число 32. В ответе укажите два числа: сначала значение искомой суммы для файла А, затем для файла B.<br><br><b>Предупреждение: </b>для обработки файла B <b>не следует </b>использовать переборный алгоритм, вычисляющий сумму для всех возможных вариантов, поскольку написанная по такому алгоритму программа будет выполняться слишком долго.',
-	'kompege.ru task:23',
-	1,
-	2,
-	'127127\000399762080'::bytea,
-	false
-);
-
-INSERT INTO kims (name) VALUES ('Тестовый вариант');
-INSERT INTO kims_tasks VALUES (4, 3, 0);
-INSERT INTO kims_tasks VALUES (4, NULL, 1);
-
-INSERT INTO groups (display_name) VALUES ('Тестовая группа');
-INSERT INTO users_groups VALUES (1, 5);
-INSERT INTO users_groups VALUES (2, 5);
-
-INSERT INTO groups_kims VALUES (
-	5,
-	4,
-	'2022-12-31 00:00:00',
-	'2023-01-31 00:00:00',
-	31::bigint * 86400 * 1000,
-	false,
-	false
-);
